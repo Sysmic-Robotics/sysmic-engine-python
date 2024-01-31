7ebd3466cd65069b3c1abfdd43ab8fc5dd53b809
Gerson <gersonhernan00@hotmail.com>
2024-01-31 08:18:00 -0300

    RRT Path planning

---
+++ python-engine/src/pathplanning/PathPlanning.py
@@ -0,0 +1,10 @@
+from RRTAlgorithm import RRTAlgorithm
+from RRTMap import RRTMap
+
+# Unica funcion que debe que llamarse de este modulo
+def GetPath(point_from, point_to):
+    rtt = RRTAlgorithm()
+    # Faltan obtener los obstaculos (robots y pelota) en una lista de coordenadas
+    #map = RRTMap(MAP_H, MAP_W,  )
+    #path = rtt.GetPath(map, point_from, point_to) 
+    #return path
