6b244e15a02bde2f65160ea755dd7d86006ae4ee
Gerson <Gerson@Gerson.com>
2024-07-04 18:02:42 -0400

    Robot robocup coordinates to local coordinates

--- new_gui/src/components/Gameplay.tsx
+++ new_gui/src/components/Gameplay.tsx
@@ -15,14 +15,24 @@ const Gameplay: React.FC<GameplayProps> = ({ width, height })  =>{
         {
           id: 1,
           team: 0,
-          position: { x: 100, y: -200 },
+          position: { x: 0, y: 0 },
+        },
+        {
+          id: 1,
+          team: 0,
+          position: { x: 100, y: 50 },
+        },
+        {
+          id: 2,
+          team: 1,
+          position: { x: 20, y: 40 },
         }
       ];
     return (
       <>
           <Field width={width} height={height}/>
           {robots.map((robot) => (
-              <Robot key={robot.id} {...robot}/>
+              <Robot key={robot.id} id={robot.id} team={robot.team} position={robot.position} field_data={{w:width, h:height}}/>
           ))}
           
           
--- new_gui/src/components/Robot.tsx
+++ new_gui/src/components/Robot.tsx
@@ -8,13 +8,22 @@ interface RobotProps {
       x: number;
       y: number;
     };
+    field_data:{
+        w:number;
+        h:number;
+    }
 }
 
+interface Position{
+    x: number;
+    y: number;
+}
 // Define the Robot component
-const Robot: React.FC<RobotProps> = ({ id, team, position }) => {
+const Robot: React.FC<RobotProps> = ({ id, team, position, field_data }) => {
     // Determine the color based on the team
     const color = team === 0 ? 'blue' : 'yellow';
-
+    // Get the local coordinates
+    let localPosition : Position = to_local_coordinates(position.x, position.y);
     // Define the styles for the circle
     const circleStyle: React.CSSProperties = {
         width: '20px',
@@ -22,14 +31,26 @@ const Robot: React.FC<RobotProps> = ({ id, team, position }) => {
         borderRadius: '50%',
         backgroundColor: color,
         position: 'relative',
-        left: `${position.x}px`,
-        top: `${position.y}px`,
+        left: `${localPosition.x}px`,
+        top: `${localPosition.y}px`,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         color: 'white',
         fontSize: '12px',
     };
+
+    // Convert robocup coords to css coords
+    function to_local_coordinates(x : number,y : number) : Position{
+        let new_x : number = x;
+        let new_y : number = y;
+        // Traslation
+        new_x += field_data.w/2 ;
+        new_y -= field_data.h/2;
+        new_x = Math.min(field_data.w, Math.max(0, new_x));
+        new_y = Math.min(0, Math.max(-field_data.h, new_y));
+        return {x : new_x, y : new_y};
+    }
 
     return <div style={circleStyle}>{id}</div>;
 };
