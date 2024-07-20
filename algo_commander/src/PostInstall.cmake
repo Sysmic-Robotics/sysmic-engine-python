93cdbbb7f553abe6d6fef31d7624eed32a0bab3d
Gerson <gersonhernan00@hotmail.com>
2024-07-20 15:29:38 -0400

    proyectos deprecados

--- algo_commander/src/PostInstall.cmake
+++
@@ -1,3 +0,0 @@
-# Remove stuff from dependencies installs which we don't need
-include(GNUInstallDirs)
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR})
--- joystick_test.sh
+++
@@ -1,3 +0,0 @@
-source test/bin/activate
-sudo chmod 666 /dev/ttyUSB0
-python radio_test_copy.py
